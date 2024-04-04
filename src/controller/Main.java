371705fe0cc9311eb989f84a2543ad8a7d7dc5be
Devin Proothi <dproothi@student.ubc.ca>
2024-04-04 13:08:45 -0700

    Fixed Setup Commit

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,4 +1,42 @@
 package controller;
 
-public class Main {
+import database.DatabaseConnectionHandler;
+import delegates.LoginWindowDelegate;
+import ui.LoginWindow;
+
+public class Main implements LoginWindowDelegate {
+    private DatabaseConnectionHandler dbHandler = null;
+    private LoginWindow loginWindow = null;
+
+
+
+    public static void main(String[] args) {
+        Main mainController = new Main();
+        mainController.start();
+    }
+
+    private void start() {
+        loginWindow = new LoginWindow();
+        loginWindow.showFrame(this);
+        dbHandler = new DatabaseConnectionHandler();
+    }
+
+    @Override
+    public void login(String username, String password) {
+        boolean didConnect = dbHandler.login(username, password);
+
+        if (didConnect) {
+            // Once connected, remove login window and start text transaction flow
+            loginWindow.dispose();
+
+        } else {
+            loginWindow.handleLoginFailed();
+
+            if (loginWindow.hasReachedMaxLoginAttempts()) {
+                loginWindow.dispose();
+                System.out.println("You have exceeded your number of allowed attempts");
+                System.exit(-1);
+            }
+        }
+    }
 }
