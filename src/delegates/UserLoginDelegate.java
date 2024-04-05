c4533d2a8e40b2923816ef909f30a662bb40f380
UBC Student <aryaan03@students.cs.ubc.ca>
2024-04-05 01:35:47 -0700

    Updated the interface for insertion of listers and seekers

--- src/delegates/UserLoginDelegate.java
+++ src/delegates/UserLoginDelegate.java
@@ -1,8 +1,15 @@
 package delegates;
 
+import model.listerModel;
+import model.seekerModel;
 import model.userModel;
+
+import java.util.List;
 
 public interface UserLoginDelegate {
     void insertUser(userModel user);
     boolean checkUserId(String userIdInput);
+    List<String> getResIdsName();
+    void insertLister(listerModel lister);
+    void insertSeeker(seekerModel seeker);
 }
