6f47497e0ab2acc006fd9404e8a67f0609d83148
UBC Student <aadityad@students.cs.ubc.ca>
2024-04-05 22:06:28 -0700

    updated delegate file

--- src/delegates/DashBoardDelegate.java
+++ src/delegates/DashBoardDelegate.java
@@ -1,7 +1,15 @@
 package delegates;
 
+import java.util.List;
+
 public interface DashBoardDelegate {
     void deleteUser(String userID);
     void updateSocialPageHas(String userID, String newEmailID, String newPhoneNumber, String newInstagramUsername);
-
+    List<List<Object>> queryResidences(String whereClause);
+    List<List<Object>> join();
+    List<List<Object>> aggGroupBy();
+    List<List<Object>> aggHavingWithJoin();
+    List<List<Object>> nestedAggGroupBy();
+    List<List<Object>> performDivisionQuery();
+    List<List<Object>> projectUser(String query);
 }
